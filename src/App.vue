38db536d3175ac6f87c1147f0c1ee032b110354c
verdantpath <m_dittmann@yahoo.com>
2019-10-06 15:01:12 -0700

    add template tags

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="app">
-    <h1>Stuff</h1>
+  <div id="app" class="container">
+    <h2>
+      <span>Pomodoro</span>
+      <controls-component></controls-component>
+    </h2>
+    <state-title-component></state-title-component>
+    <countdown-component></countdown-component>
+    <kittens-component></kittens-component>
   </div>
 </template>
 
