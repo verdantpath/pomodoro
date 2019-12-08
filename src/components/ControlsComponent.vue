b65bc2583b6fa06e5d3aaa6c47c9aaf358a4ada3
verdantpath <m_dittmann@yahoo.com>
2019-12-08 11:55:03 -0800

    add mutations, getters and actions

--- src/components/ControlsComponent.vue
+++ src/components/ControlsComponent.vue
@@ -1,19 +1,28 @@
 <template>
   <div>
-    <button>
+    <button :disabled='isStarted && !isPaused' @click="start">
       <i class="glyphicon glyphicon-play"></i>
     </button>
-    <button>
+    <button :disabled='!isStarted || isPaused' @click="pause">
       <i class="glyphicon glyphicon-pause"></i>
     </button>
-    <button>
+    <button :disabled='!isStarted' @click="stop">
       <i class="glyphicon glyphicon-stop"></i>
     </button>
   </div>
 </template>
 
 <style scoped>
+button:disabled i {
+  color: gray;
+}
 </style>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: mapGetters(['isStarted', 'isPaused', 'isStopped']),
+  methods: mapActions(['start', 'stop', 'pause'])
+}
 </script>
